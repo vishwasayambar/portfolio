3df8df142d9c983e1894a3e65eb330fe74833b8d
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-04-29 00:53:05 +0530

    created All Required coponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperianceComponent } from './components/experiance/experiance.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    ExperianceComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/experiance/experiance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiance',
+  templateUrl: './experiance.component.html',
+  styleUrls: ['./experiance.component.scss']
+})
+export class ExperianceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
