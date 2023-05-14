85ae9d8184fbaea61a781694dbc7babf39bc62cd
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-05-14 17:38:32 +0530

    Created New Modules for all Material modules and for all constants

---
+++ src/app/module/angular-material/angular-material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+
+const MaterialComponents = [
+  MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialComponents,
+  ],
+  exports: [MaterialComponents]
+})
+export class AngularMaterialModule { }
---
+++ src/app/module/constants/constants.module.ts
@@ -0,0 +1,64 @@
+export interface skill {
+  name: string;
+  image: string;
+  category: string;
+}
+
+export interface project {
+  name: string;
+  image: string;
+  techstack: string;
+  category: string;
+  links: {
+    visit: string;
+    code: string;
+    video: string;
+  };
+}
+
+export interface experience {
+  company: string;
+  position: string;
+  duration: string;
+  desc: string[];
+}
+
+export interface education {
+  institute: string;
+  degree: string;
+  duration: string;
+  desc: string[];
+}
+
+export interface main {
+  name: string;
+  titles: string[];
+  heroImage: string;
+  shortDesc: string;
+  techStackImages: string[];
+}
+
+export interface about {
+  aboutImage: string;
+  aboutImageCaption: string;
+  title: string;
+  about: string;
+  resumeUrl: string;
+  callUrl: string;
+}
+
+export interface social {
+  icon: string;
+  link: string;
+}
+
+export interface data {
+  [x: string]: any;
+  main: main;
+  about: about;
+  skills: skill[];
+  projects: project[];
+  experiences: experience[];
+  educations: education[];
+  socials: social[];
+}
