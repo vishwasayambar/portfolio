7ce6801a1fe4549da4c06404d770ea58e5c67fcd
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-08 00:12:40 +0530

    Update Experience component

--- src/app/components/experiance/experiance.component.ts
+++ src/app/components/experiance/experiance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-experiance',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperianceComponent implements OnInit {
 
+  @Input() experiences;
   constructor() { }
+  isView = false;
+  viewAllCard= 2;
+  category = 'Experience';
+  defaultCards = [];
+
 
   ngOnInit(): void {
+    this.defaultCards = this.experiences.slice(0,2);
+  }
+
+  isViewALlExperience(){
+    this.isView = true;
+    this.defaultCards = [...this.experiences];
+    console.log(this.defaultCards, "View all experiences");
+    console.log(this.experiences, "View all experiences");
+  }
+  isCloseALlExperience(){
+    this.isView = false;
+    console.log(this.defaultCards, "close all experiences");
+    this.defaultCards.splice(2);
+    console.log(this.defaultCards, "close all experiences");
+    console.log(this.experiences, "close all experiences wdjhfjkkj");
+  }
+  onChange(change){
+    if(change === 'Experience'){
+      this.category = "Experience";
+    }else {
+      this.category = "Education";
+    }
   }
 
 }
