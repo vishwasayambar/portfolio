97dae35d519f7efc5d29ad64e4fd3e6919ea872b
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-08 09:05:59 +0530

    Feat: Added Education and also did code reformating

--- src/app/components/experiance/experiance.component.ts
+++ src/app/components/experiance/experiance.component.ts
@@ -1,43 +1,47 @@
 import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-experiance',
-  templateUrl: './experiance.component.html',
-  styleUrls: ['./experiance.component.scss']
+	selector: "app-experiance",
+	templateUrl: "./experiance.component.html",
+	styleUrls: ["./experiance.component.scss"]
 })
 export class ExperianceComponent implements OnInit {
 
-  @Input() experiences;
-  constructor() { }
-  isView = false;
-  viewAllCard= 2;
-  category = 'Experience';
-  defaultCards = [];
+	@Input() experiences;
+	@Input() educations;
+
+	constructor() {
+	}
+
+	isView = false;
+	category = "Experience";
+	defaultExperienceCards = [];
+	defaultEducationCards = [];
 
 
-  ngOnInit(): void {
-    this.defaultCards = this.experiences.slice(0,2);
-  }
+	ngOnInit(): void {
+		this.defaultExperienceCards = this.experiences.slice(0, 2);
+		this.defaultEducationCards = this.educations.slice(0, 2);
+	}
 
-  isViewALlExperience(){
-    this.isView = true;
-    this.defaultCards = [...this.experiences];
-    console.log(this.defaultCards, "View all experiences");
-    console.log(this.experiences, "View all experiences");
-  }
-  isCloseALlExperience(){
-    this.isView = false;
-    console.log(this.defaultCards, "close all experiences");
-    this.defaultCards.splice(2);
-    console.log(this.defaultCards, "close all experiences");
-    console.log(this.experiences, "close all experiences wdjhfjkkj");
-  }
-  onChange(change){
-    if(change === 'Experience'){
-      this.category = "Experience";
-    }else {
-      this.category = "Education";
-    }
-  }
+	isViewALlExperience() {
+		this.isView = true;
+		this.defaultExperienceCards = [...this.experiences];
+		this.defaultEducationCards = [...this.educations];
+	}
+
+	isCloseALlExperience() {
+		this.isView = false;
+		this.defaultExperienceCards.splice(2);
+		this.defaultEducationCards.splice(2);
+	}
+
+	onChange(change) {
+		if (change === "Experience") {
+			this.category = "Experience";
+		} else {
+			this.category = "Education";
+		}
+	}
 
 }
