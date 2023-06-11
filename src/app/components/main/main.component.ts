10a361ff6a01fd50f6ad30dd541abeb9bbd31b53
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-11 15:40:57 +0530

    Impr: Added typeWritingEffect for subheading

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+	selector: "app-main",
+	templateUrl: "./main.component.html",
+	styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
-  isMobileDevice = window.innerWidth < 720;
-  constructor() { }
+	isMobileDevice = window.innerWidth < 720;
+	developerNames = ["Frontend Developer", "Backend Developer", "Fullstack Developer",];
+	currentTextIndex = 0; // Current Text Index of the Array
+	displayText = ''; // Text to display
 
-  ngOnInit(): void {
-  }
+	constructor() {
+	}
+
+	ngOnInit(): void {
+      this.typeEffect();
+	}
+
+	typeEffect() {
+		const currentText = this.developerNames[this.currentTextIndex]; // current Text gets from array using current index
+		const currentTextLength = currentText.length; //Current Text Length
+		let i = 0; // Increment required for traversing current texts character on by one
+
+		const typewritingEffect = setInterval(() => {
+			if (i < currentTextLength) {
+				this.displayText += currentText.substring(i, i + 1);
+				if(this.displayText.length === currentTextLength) {
+					setTimeout(() =>{},200);
+				}
+				i++;
+			} else {
+				clearInterval(typewritingEffect);
+				setTimeout(() => {
+					this.currentTextIndex++;
+					if (this.currentTextIndex >= this.developerNames.length) {
+						this.currentTextIndex = 0;
+					}
+					this.typeEffect();
+					this.displayText = '';
+				}, 2000);
+			}
+		}, 100);
+
+	}
+
 
 }
