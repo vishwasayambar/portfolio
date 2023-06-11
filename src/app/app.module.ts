e86ce348ab41f293edfeee64cc86c466b6103a25
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-11 15:38:56 +0530

    Ddclare Contact component in App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,43 @@
 import { NgOptimizedImage } from "@angular/common";
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
-import { AboutComponent } from './components/about/about.component';
-import { ExperianceComponent } from './components/experiance/experiance.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./components/main/main.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ExperianceComponent } from "./components/experiance/experiance.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularMaterialModule } from "./module/angular-material/angular-material.module";
-import { SkillsComponent } from './components/skills/skills.component';
-import { SocialComponent } from './components/social/social.component';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from "./components/skills/skills.component";
+import { SocialComponent } from "./components/social/social.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    AboutComponent,
-    ExperianceComponent,
-    ContactComponent,
-    FooterComponent,
-    SkillsComponent,
-    SocialComponent,
-    ProjectsComponent
-  ],
+	declarations: [
+		AppComponent,
+		MainComponent,
+		AboutComponent,
+		ExperianceComponent,
+		ContactComponent,
+		FooterComponent,
+		SkillsComponent,
+		SocialComponent,
+		ProjectsComponent,
+		ContactComponent
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		AngularMaterialModule,
 		BrowserAnimationsModule,
 		NgOptimizedImage,
+		FormsModule
 	],
-  providers: [],
-  bootstrap: [AppComponent]
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
