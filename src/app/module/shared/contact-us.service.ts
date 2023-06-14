6832d78cc5e7bdc8d70e447132a82dda37da21f9
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-15 01:11:09 +0530

    Created New Service for Contact us form submit

---
+++ src/app/module/shared/contact-us.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactUsService {
+   url = 'https://formspree.io/f/mjvdnwrn';
+
+  constructor(public http:HttpClient) {
+
+  }
+
+  formSubmit(params: any) {
+    return this.http.post(this.url, params);
+  }
+}
