5f97ccd7c661f76ec7ede31bc23a194777e11095
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-16 00:05:57 +0530

    Added Validator and popup for contact form submit

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Notify } from "notiflix";
 import { ContactUsService } from "../../module/shared/contact-us.service";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+	selector: "app-contact",
+	templateUrl: "./contact.component.html",
+	styleUrls: ["./contact.component.scss"]
 })
 export class ContactComponent implements OnInit {
-  isMobileDevice = window.innerWidth < 920;
+	isMobileDevice = window.innerWidth < 920;
 
-  constructor(protected service:ContactUsService) { }
+	constructor(protected service: ContactUsService) {
 
-  ngOnInit(): void {
-  }
-  save(data:NgForm){
+	}
 
-    this.service.formSubmit(data).subscribe(response => {
-      console.log(response);
-      alert('Form saved successfully');
-    }, err => {
-      console.log(err);
-    });
-    // console.log("Form Data",data)
-  }
+	ngOnInit(): void {
+	}
 
+	save(form: NgForm) {
+		this.service.formSubmit(form).subscribe(response => {
+			Notify.success("Form submitted successfully! 🎉");
+			form.reset();
+		}, err => {
+			Notify.failure(err);
+		});
+	}
+
+	protected readonly eval = eval;
+	protected readonly event = event;
 }
