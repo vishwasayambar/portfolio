0987f0418f015c751a8bffd72d0d4af66ac8b2d0
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-20 23:38:53 +0530

    Added PWA package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgOptimizedImage } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -17,6 +17,7 @@ import { ResumeComponent } from "./components/resume/resume.component";
 import { SkillsComponent } from "./components/skills/skills.component";
 import { SocialComponent } from "./components/social/social.component";
 import { AngularMaterialModule } from "./module/angular-material/angular-material.module";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -42,6 +43,12 @@ import { AngularMaterialModule } from "./module/angular-material/angular-materia
 		NgOptimizedImage,
 		FormsModule,
 		HttpClientModule,
+  ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    // Register the ServiceWorker as soon as the application is stable
+    // or after 30 seconds (whichever comes first).
+    registrationStrategy: 'registerWhenStable:30000'
+  }),
 	],
 	providers: [],
 	bootstrap: [AppComponent]
