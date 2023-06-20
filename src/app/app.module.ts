3d24c3fe3c2b23b3db6f42638ca6fd8a39b40738
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-06-20 23:56:38 +0530

    Fix: Cannot enable prod mode after platform setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgOptimizedImage } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule, isDevMode } from "@angular/core";
+import { isDevMode, NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment.prod";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AboutComponent } from "./components/about/about.component";
@@ -17,7 +19,6 @@ import { ResumeComponent } from "./components/resume/resume.component";
 import { SkillsComponent } from "./components/skills/skills.component";
 import { SocialComponent } from "./components/social/social.component";
 import { AngularMaterialModule } from "./module/angular-material/angular-material.module";
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -43,12 +44,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 		NgOptimizedImage,
 		FormsModule,
 		HttpClientModule,
-  ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: !isDevMode(),
-    // Register the ServiceWorker as soon as the application is stable
-    // or after 30 seconds (whichever comes first).
-    registrationStrategy: 'registerWhenStable:30000'
-  }),
+		ServiceWorkerModule.register("ngsw-worker.js", {
+			enabled: environment.production,
+			// Register the ServiceWorker as soon as the application is stable
+			// or after 30 seconds (whichever comes first).
+			registrationStrategy: "registerWhenStable:30000"
+		}),
 	],
 	providers: [],
 	bootstrap: [AppComponent]
